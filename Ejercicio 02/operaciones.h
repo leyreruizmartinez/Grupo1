e7efcdce97a466491799d51cf2dd6efa4769f127
Leyre Ruiz <leyre.ruiz.martinez@opendeusto.es>
2025-03-24 00:34:44 +0100

    Eliminando carpetas innecesarias

--- Ejercicio 02/operaciones.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPERACIONES_H
-#define OPERACIONES_H
-
-double sumar(double a, double b);
-double restar(double a, double b);
-int multiplicar(int a, int b);
-int potencia(int base, int exponente);
-
-#endif
