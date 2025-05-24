8eae221970be4ac0a2692a712e1af8e2a95624cd
Leyre Ruiz <leyre.ruiz.martinez@opendeusto.es>
2025-05-24 04:44:35 +0200

    por fin

--- Proyecto/src/prestamo.h
+++ Proyecto/src/prestamo.h
@@ -24,6 +24,8 @@ int devolver_libro(int id_usuario, const char* isbn);
 int pedir_libro_remoto(sqlite3 *db, int id_usuario, const char* isbn);
 int devolver_libro_remoto(sqlite3 *db, int id_usuario, const char* isbn);
 void calcular_fecha_devolucion(const char* fecha_prestamo, char* fecha_devolucion, size_t size);
+int calcular_diferencia_dias(const char* fecha1, const char* fecha2);
+void obtener_fecha_actual(char* buffer, size_t size);
 
 // Funciones auxiliares
 int contar_prestamos_atrasados(int id_usuario);
